26d84f362af2f8b44db8686b82c58d3b75141a10
anclko <ackoch@hotmail.com>
2024-01-05 14:29:23 +0100

    Creating arrays in 3 different steps and then in a combined steps

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //creating an array
+        //Step 1: Declare, creates a reference to array
+        //Step 2: Instantiation of an array, creates an array
+        //Step 3: assigns values to cells in array
+
+        //Step 1
+        int[] intArray;
+
+        //Step 2
+        intArray = new int[3];
+
+        //Step 3
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+
+        System.out.println(Arrays.toString(intArray));
+
+        //Combined steps
+        String sArray[] = {"a", "b", "c"};
+        System.out.println(Arrays.toString(sArray));
+        }
+}
