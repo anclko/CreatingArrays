dcce39af62ede72618a661ab76619e08054d60cd
anclko <ackoch@hotmail.com>
2024-01-05 14:29:56 +0100

    Creating arrays in 3 different steps and then in a combined steps

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
         System.out.println(Arrays.toString(intArray));
 
         //Combined steps
-        String sArray[] = {"a", "b", "c"};
+        String[] sArray = {"a", "b", "c"};
         System.out.println(Arrays.toString(sArray));
         }
 }
